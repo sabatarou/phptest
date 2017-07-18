4b6bb97bfb1ab20b9266c0e4b144afc21828df6b
ayatakamaru <terawaku51@gmail.com>
2017-07-18 10:09:20 +0900

    vender.php 追加 #015

---
+++ test/vendingmachine/vender.php
@@ -0,0 +1,18 @@
+<!Doctype HTML>
+<html>
+ <head>
+  <meta charset=utf-8>
+  <title>自動販売機</title>
+</head>
+  <body>
+    <form method="post">
+    <button type="submit" name="choice" value="10">10円</button>
+    <button type="submit" name="choice" value="50">50円</button>
+    <button type="submit" name="choice" value="100">100円</button>
+  </form>
+    <?php
+    $s+=htmlspecialchars($_POST['choice'], ENT_QUOTES, 'UTF-8');
+    print("$s");
+     ?>
+  </body>
+</html>
