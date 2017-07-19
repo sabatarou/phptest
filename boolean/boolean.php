b44cc27918454112bde45d32e79717f6009dd15f
ayatakamaru <terawaku51@gmail.com>
2017-07-19 23:51:46 +0900

    boolean.php 追記　#026

--- boolean/boolean.php
+++ boolean/boolean.php
@@ -2,11 +2,15 @@
 //==は,boolean型を表すを返す演算子
 $version="1.23";
 ($version=="1.23")?print("バージョンは1.23です<br>"):print("バージョンは1.23ではありません<br>");
-$show_line=false;
-var_dump($show_line);
+$show_line=true;
 if($show_line)
 {
-  echo("線は表示されません");
+  echo "<hr>",var_dump($show_line),"線が表示されます","<br>";
 }
-$show_line="";
+$show_line=false;
+if(!$show_line)
+{
+  echo var_dump($show_line),"線は表示されません";
+}
+
  ?>
