135479cdaf554c15aee4bbdac7693b55124b6ffc
Ayatakamaru <terawaku51@gmail.com>
2017-07-19 17:23:07 +0900

    vender.php　追記

--- test/Vender/vender.php
+++ test/Vender/vender.php
@@ -9,34 +9,52 @@
       session_start();
       if (!isset($_SESSION["money"]))
       {
-          $_SESSION["money"] = 1000;
+          $_SESSION["money"] = 0;
       }
-      if (isset($_POST["gold"])&&$_SESSION["money"]>=100)
+      if (isset($_POST["item"])&&$_SESSION["money"]>=$_POST['item'])
       {
-          $_SESSION["money"]-=100;
-          echo"エナジーゴールドを買いました";
+          $_SESSION["money"]-=$_POST['item'];
+          echo"商品をを買いました";
+          if($_SESSION["money"]<100&&$_SESSION["money"]!=0)
+          {
+            print($_SESSION["money"]."円お釣りを出しました");
+            $_SESSION["money"] =0;
+          }
       }
-      else if (isset($_POST["noguti"]))
+      elseif(isset($_POST["item"])&&$_SESSION["money"]<$_POST['item'])
       {
-          $_SESSION["money"] += 1000;
-          echo"野口英世を入れました";
+          print("お金が足りません");
       }
-      else if (isset($_POST["reset"]))
+      if (isset($_POST["gold"]))
+      {
+          $_SESSION["money"]+=$_POST['gold'];
+          print("お金を入れました");
+      }
+      if (isset($_POST["reset"]))
       {
         print($_SESSION["money"]."円お釣りを出しました");
-          $_SESSION["money"] =0;
-
+        $_SESSION["money"] =0;
       }
-      print("<br>");
-      print($_SESSION["money"]);
+      print("<br>".$_SESSION["money"]."円");
   ?>
 
   <form method="POST" action="">
-      <input type="submit" name="gold" value="エナジーゴールド">
-      <br>
-      <input type="submit" name="noguti" value="野口 英世ボタン">
-      <br>
-      <input type="submit" name="reset" value="おつり返却">
+    <br>
+      <p>商品<p/>
+      <button type="submit" name="item" value="100">リアルシルバー</button>
+      <button type="submit" name="item" value="110">いのはす</button>
+      <button type="submit" name="item" value="150">あたやか</button>
+      <button type="submit" name="item" value="120">ペペシ</button>
+    <br>
+      <p>現金<p/>
+      <button type="submit" name="gold" value="10">10円</button>
+      <button type="submit" name="gold" value="50">50円</button>
+      <button type="submit" name="gold" value="100">100円</button>
+      <button type="submit" name="gold" value="500">500円</button>
+      <button type="submit" name="gold" value="1000">1000円</button>
+    <br>
+    <p>お釣り<p/>
+      <button type="submit" name="reset">お釣り</button>
   </form>
   </body>
 </html>
