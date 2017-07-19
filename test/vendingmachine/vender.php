b97a4628936d8a75169c5b92c1f96131b815a321
ayatakamaru <terawaku51@gmail.com>
2017-07-19 14:44:42 +0900

    vender　ボタン追加 #017

--- test/vendingmachine/vender.php
+++ test/vendingmachine/vender.php
@@ -5,10 +5,38 @@
   <title>自動販売機</title>
 </head>
   <body>
-    <form method="post">
-    <button type="submit" name="choice" value="10">10円</button>
-    <button type="submit" name="choice" value="50">50円</button>
-    <button type="submit" name="choice" value="100">100円</button>
+    <?php
+      session_start();
+      if (!isset($_SESSION["money"]))
+      {
+          $_SESSION["money"] = 1000;
+      }
+      if (isset($_POST["gold"])&&$_SESSION["money"]>=100)
+      {
+          $_SESSION["money"]-=100;
+          echo"エナジーゴールドを買いました";
+      }
+      else if (isset($_POST["noguti"]))
+      {
+          $_SESSION["money"] += 1000;
+          echo"野口英世を入れました";
+      }
+      else if (isset($_POST["reset"]))
+      {
+        print($_SESSION["money"]."円お釣りを出しました");
+          $_SESSION["money"] =0;
+
+      }
+      print("<br>");
+      print($_SESSION["money"]);
+  ?>
+
+  <form method="POST" action="">
+      <input type="submit" name="gold" value="エナジーゴールド">
+      <br>
+      <input type="submit" name="noguti" value="野口 英世ボタン">
+      <br>
+      <input type="submit" name="reset" value="おつり返却">
   </form>
   </body>
 </html>
